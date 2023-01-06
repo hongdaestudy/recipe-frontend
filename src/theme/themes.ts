e6054508fdba861a0f7843786740891e3512cece
MrChanghao <lnl2048@gmail.com>
2023-01-06 18:16:01 +0900

    @material ui 테마 에러 fix

--- src/theme/themes.ts
+++ src/theme/themes.ts
@@ -1,8 +1,20 @@
-export const theme = {
-  // color
-  text: '#304156',
-  white: '#FFFFFF',
-  skyBlue: '#2EB7FF',
-};
+import { createTheme } from '@mui/material/styles';
+
+const fontSize = 14; // px
+// Tell Material-UI what's the font-size on the html element.
+// 16px is the default font-size used by browsers.
+const htmlFontSize = 16;
+const coef = fontSize / 14;
+
+export const theme = createTheme({
+  // typography: {
+  //   pxToRem: (size: number) => `${(size / htmlFontSize) * coef}rem`,
+  // },
+  // palette: {
+  //   text: '#304156',
+  //   white: '#FFFFFF',
+  //   skyBlue: '#2EB7FF',
+  // },
+});
 
 export type Theme = typeof theme;
